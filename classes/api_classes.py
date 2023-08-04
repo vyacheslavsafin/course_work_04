6f76d5c25f6ed2b79b6db02b510d3281944c8f0a
vyacheslavsafin <safin.vr@mail.ru>
2023-08-05 00:46:56 +0300

    класс HeadHunterAPI наследник абстрактного класса

--- classes/api_classes.py
+++ classes/api_classes.py
@@ -16,3 +16,30 @@ class GetAPI(ABC):
     @abstractmethod
     def get_vacancies(self, query: str) -> dict:
         pass
+
+class HeadHunterAPI(GetAPI):
+    """
+    класс для работы с HeadHunter API
+    """
+
+    def get_vacancies(self, query: str) -> dict:
+        print(f'\nПолучение данных с {HEAD_HUNTER_URL}...')
+        params = {
+            'text': f'NAME:{query}',
+            'page': 0,
+            'per_page': 100,
+            'only_with_salary': True
+        }
+        response = requests.get(HEAD_HUNTER_URL, params)
+        result_page = response.json()
+        self.vacancies = result_page['items']
+        while len(result_page['items']) == 100:
+            print(f"Загружено страниц c вакансиями: {params['page'] + 1}")
+            params['page'] += 1
+            response = requests.get(HEAD_HUNTER_URL, params)
+            result_page = response.json()
+            if result_page.get('items'):
+                self.vacancies.extend(result_page['items'])
+            else:
+                break
+        return self.vacancies
