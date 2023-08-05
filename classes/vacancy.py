ebeddcf7a9a2bd8a8ab41fa6f006bdd99ccbc0d8
vyacheslavsafin <safin.vr@mail.ru>
2023-08-05 13:48:01 +0300

    добавлен класс метод finally_sorted

--- classes/vacancy.py
+++ classes/vacancy.py
@@ -2,7 +2,9 @@ class Vacancy:
     """
     Класс для работы с вакансиями
     """
-    def __init__(self, profession, published_date, salary_from, salary_to, currency, description, responsibility, company, url):
+
+    def __init__(self, profession, published_date, salary_from, salary_to, currency, description, responsibility,
+                 company, url):
         self.profession = profession
         self.published_date = published_date
         self.salary_from = salary_from
@@ -12,3 +14,42 @@ class Vacancy:
         self.responsibility = responsibility
         self.company = company
         self.url = url
+
+    @classmethod
+    def finally_sorted(cls, vacancies):
+        top = []
+        for vac in vacancies:
+            vacancy = cls(vac["profession"],
+                          vac["published_date"],
+                          vac["salary_from"],
+                          vac["salary_to"],
+                          vac["currency"],
+                          vac["description"],
+                          vac["responsibility"],
+                          vac["company"],
+                          vac["url"]
+                          )
+            top.append(vacancy)
+        return top
+
+    def __repr__(self):
+        return (f"Vacancy(profession='{self.profession}',"
+                f"published_date='{self.published_date}',"
+                f"salary_from='{self.salary_from}',"
+                f"salary_to='{self.salary_to}',"
+                f"currency='{self.currency}',"
+                f"description='{self.description}',"
+                f"responsibility='{self.responsibility}',"
+                f"company='{self.company}',"
+                f"url='{self.url}')")
+
+    def __str__(self):
+        return (f"(profession='{self.profession}',"
+                f"published_date='{self.published_date}',"
+                f"salary_from='{self.salary_from}',"
+                f"salary_to='{self.salary_to}',"
+                f"currency='{self.currency}',"
+                f"description='{self.description}',"
+                f"responsibility='{self.responsibility}',"
+                f"company='{self.company}',"
+                f"url='{self.url}')")
