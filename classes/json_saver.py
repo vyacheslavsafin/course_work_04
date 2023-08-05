9e4b88049eaeeef4fe2b9cd09baf550d586daf32
vyacheslavsafin <safin.vr@mail.ru>
2023-08-05 13:00:11 +0300

    Добавлены методы в класс JSONSaver для чтения/записи JSON файлов

--- classes/json_saver.py
+++ classes/json_saver.py
@@ -18,6 +18,20 @@ class JSONSaver(Saver):
     """
 
     def to_json(self, file, dict_):
-        """Метод, сохраняющий в файл значения атрибутов экземпляра класса"""
         with open(file, 'w', encoding='UTF-8') as fp:
             fp.write(json.dumps(dict_, indent=4, ensure_ascii=False))
+
+    def load_from_json(self, file):
+        with open(file, 'r') as fp:
+            return json.load(fp)
+
+    def vacancies_to_json(self, file, vacancies):
+        dict_vacancies = []
+        for vacancy in vacancies:
+            if vacancy.responsibility is None:
+                vacancy.responsibility = ''
+            if vacancy.description is None:
+                vacancy.description = ''
+            dict_vacancies.append(vacancy.__dict__)
+        with open(file, 'w', encoding='UTF-8') as fp:
+            fp.write(json.dumps(dict_vacancies, indent=4, ensure_ascii=False))
